db537f18a347b2aeee41dcd58668f6ba3047b39d
Jangid <saurabh.jangid@accenture.com>
2021-11-23 17:21:19 +0530

    practice files

---
+++ ForDemo.java
@@ -0,0 +1,13 @@
+package com.accenture.lkm.simplejavaprograms;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n=10;
+       for(int i=1;i<=10;i++) {  // 1<=10; 2<=10; 11<=10
+    	   System.out.println(" i : "+i);
+       }
+	}
+
+}
---
+++ IfElseDemo.java
@@ -0,0 +1,15 @@
+package com.accenture.lkm.simplejavaprograms;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int mark=60;
+      if(mark>=70) {
+    	  System.out.println("Pass");
+      }else {
+    	  System.out.println("Reappear");
+      }
+	}
+
+}
